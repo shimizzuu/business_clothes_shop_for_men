3b3ee1e3ab24bcd636e5d6a4d0d7ebc1a99e2413
清水雄大 <shimizuyuta@Yutas.local>
2019-11-16 23:41:06 +0900

    Install RSpec

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,5 @@
 Spree::Core::Engine.load_seed if defined?(Spree::Core)
 Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
+
+Spree::Core::Engine.load_seed if defined?(Spree::Core)
+Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
