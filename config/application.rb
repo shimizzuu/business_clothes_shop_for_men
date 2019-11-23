4e182de4009e249dd204a1705f4ec13fa6206cfc
清水雄大 <shimizuyuta@Yutas.local>
2019-11-23 16:20:51 +0900

    rails-controller-testingをGemfileに追加

--- config/application.rb
+++ config/application.rb
@@ -53,10 +53,13 @@ module Potepanec
     config.generators.helper         = false
     config.generators do |g|
       g.test_framework :rspec,
-        view_specs: false,
-        helper_specs: false,
-        controller_specs: true, 
-        routing_specs: false
+      fixtures: true,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false,
+      controller_specs: true,
+      request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
   end
 end
