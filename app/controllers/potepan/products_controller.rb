c1f95515bbd29b86b767d75c32c7baf51dfde8aa
清水雄大 <shimizuyuta@Yutas.local>
2019-11-23 16:57:13 +0900

    商品詳細ページとテストの追加

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,4 +1,7 @@
 class Potepan::ProductsController < ApplicationController
   def show
+    @product = Spree::Product.find(params[:id])
+    @images = @product.images
+    render 'potepan/products/show'
   end
 end
--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Potepan::ProductsController, type: :controller do
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+  describe 'GET #show' do
+    before do
+      @product = create(:product)
+      get :show, params: { id: @product.id }
+    end
+
+    it "@productが取得できていること" do
+      expect(assigns[:product]).to eq @product
+    end
+
+    it "showページにリダイレクトされていること" do
+      expect(response).to render_template :show
     end
   end
 
