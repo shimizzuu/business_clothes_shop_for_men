7dd91ef515dbba783cef18ac10afeaf22deffa96
清水雄大 <shimizuyuta@Yutas.local>
2019-12-01 13:52:07 +0900

    カテゴリーページを追加

---
+++ app/controllers/potepan/categories_controller.rb
@@ -0,0 +1,10 @@
+class Potepan::CategoriesController < ApplicationController
+  def show
+    # サイドバーに表示するカテゴリー情報の取得
+    @categories = Spree::Taxonomy.includes(:taxons)
+
+    # 表示する商品情報の取得
+    @taxon = Spree::Taxon.find(params[:id])
+    @products = @taxon.all_products.includes(master: [:default_price, :images])
+  end
+end
