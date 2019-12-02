12a22b7020bb7ddf1131e11c708027425c68b513
清水雄大 <shimizuyuta@Yutas.local>
2019-12-02 13:44:26 +0900

    html,jsそれぞれのフォーマットでレスポンスを返せるように変更

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,9 +2,12 @@ class Potepan::CategoriesController < ApplicationController
   def show
     # サイドバーに表示するカテゴリー情報の取得
     @categories = Spree::Taxonomy.includes(:taxons)
-
     # 表示する商品情報の取得
     @taxon = Spree::Taxon.find(params[:id])
     @products = @taxon.all_products.includes(master: [:images, :default_price])
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
