2d6f214072891ddd5d1c6aac3d2c490f0380690d
清水雄大 <shimizuyuta@Yutas.local>
2019-12-02 22:55:40 +0900

    featureテスト修正, テンプレートのerror修正

--- spec/features/potepan/categories_spec.rb
+++ spec/features/potepan/categories_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
 RSpec.feature "Potepan::Categories", type: :feature do
   given!(:taxonomy) { create :taxonomy, name: 'Category' }
-
   given!(:taxon_1) { create :taxon, name: 'Bags', taxonomy: taxonomy, parent_id: taxonomy.root.id }
   given!(:taxon_2) { create :taxon, name: 'Mugs', taxonomy: taxonomy, parent_id: taxonomy.root.id }
-
   given!(:product_1) { create :product, taxons: [taxon_1] }
   given!(:product_2) { create :product, taxons: [taxon_1] }
   given!(:product_3) { create :product, taxons: [taxon_2] }
@@ -14,7 +12,7 @@ RSpec.feature "Potepan::Categories", type: :feature do
   end
 
   scenario "テンプレート表示確認" do
-    expect(page).to have_title taxon_1.name
+    expect(page).to have_title "#{taxon_1.name} - BIGBAG Store"
     expect(page).to have_selector 'h2', text: taxon_1.name.upcase
   end
 
@@ -27,13 +25,11 @@ RSpec.feature "Potepan::Categories", type: :feature do
   end
 
   scenario "toggle動作確認" do
-    context "toggleを展開していない場合" do
-      expect(find('a[data-target="#category-0"]')["aria-expanded"]).to be_falsey
-    end
-    context "toggleを展開した場合" do
-      click_on taxonomy.name
-      expect(find('a[data-target="#category-0"]')["aria-expanded"]).to be_truthly
-    end
+    # toggleを展開していない場合
+    expect(find('a[data-target="#category-0"]')["aria-expanded"]).to be_falsey
+    # toggleを展開した場合
+    click_on taxonomy.name
+    expect(find('a[data-target="#category-0"]')["aria-expanded"]).to be_truthy
   end
 
   scenario "taxon_1の商品一覧表示確認" do
@@ -51,7 +47,7 @@ RSpec.feature "Potepan::Categories", type: :feature do
     within "#productsList" do
       click_on product_1.name
     end
-    expect(page).to have_content product_1.name.upcase
+    expect(page).to have_selector 'h2', text: product_1.name.upcase
   end
 
   scenario "taxon_2の商品一覧表示確認" do
