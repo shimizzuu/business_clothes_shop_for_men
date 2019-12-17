df027d8d5f87c73fdb0f15b0ec86ed2a37ca17ec
清水雄大 <shimizuyuta@Yutas.local>
2019-12-17 22:16:20 +0900

    商品詳細ページに関連商品を表示

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,6 +1,14 @@
 class Potepan::ProductsController < ApplicationController
+  RELATED_ITEMS_DISPLAYED = 4
+
   def show
     @product = Spree::Product.find(params[:id])
     @images = @product.images
+    @related_products = Spree::Product.
+      in_taxons(@product.taxons).
+      includes(master: [:images, :default_price]).
+      where.not(id: @product.id).
+      distinct.
+      limit(RELATED_ITEMS_DISPLAYED)
   end
 end
