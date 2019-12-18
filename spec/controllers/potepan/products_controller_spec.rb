ee3ac7eb3429948cf31a3263e457bf34c89cdf8e
清水雄大 <shimizuyuta@Yutas.local>
2019-12-19 00:07:29 +0900

    products_controller_specにインスタンス変数取得テストを追加

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -1,14 +1,26 @@
 require 'rails_helper'
 RSpec.describe Potepan::ProductsController, type: :controller do
   describe 'GET #show' do
-    let(:product_1) { create :product }
+    let(:taxonomy_1) { create :taxonomy, name: 'Categories' }
+    let(:taxon_1) { create :taxon, taxonomy: taxonomy_1, parent_id: taxonomy_1.root.id }
+    let(:taxon_2) { create :taxon, taxonomy: taxonomy_1, parent_id: taxonomy_1.root.id }
+    let!(:product) { create(:product, taxons: [taxon_1]) }
+    let!(:product_same_taxon) { create(:product, taxons: [taxon_1]) }
 
     before do
-      get :show, params: { id: product_1.id }
+      get :show, params: { id: product.id }
     end
 
     it "productが取得できていること" do
-      expect(assigns[:product]).to eq product_1
+      expect(assigns[:product]).to eq product
+    end
+
+    it "imageが取得できていること" do
+      expect(assigns[:images]).to eq product.images
+    end
+
+    it "関連商品が取得できていること" do
+      expect(assigns[:related_products]).to include product_same_taxon
     end
 
     it "ステータスコード 200 OK確認" do
