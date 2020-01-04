818824ac4acc9e3f34401a5a5f4265bf8a496ebb
清水雄大 <shimizuyuta@Yutas.local>
2020-01-04 09:25:56 +0900

    helperの誤記修正

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -2,7 +2,7 @@ module CategoriesHelper
   def variants_count_on_option(option:, taxon:)
     Spree::Variant.
       joins(:option_values).
-      where(produt_id: taxon.products.ids).
+      where(product_id: taxon.products.ids).
       where("spree_option_values.id": option.id).
       count
   end
