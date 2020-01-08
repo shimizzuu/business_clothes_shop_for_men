b27573e0f0aba99627d3da1a668c0ed607dec126
清水雄大 <shimizuyuta@Yutas.local>
2020-01-08 23:21:27 +0900

    ワード検索機能を追加

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -6,4 +6,18 @@ class Potepan::ProductsController < ApplicationController
     @images = @product.images
     @related_products = Spree::Product.related_products(@product).limit(MAXIMUM_NUMBER_OF_RELATED_PRODUCTS)
   end
+
+  def search
+    @keywords = params[:keywords]
+
+    if @keywords.blank?
+      @products = []
+    else
+      @products = Spree::Product.
+        where("spree_products.name LIKE :keyword", keyword: "%#{ActiveRecord::Base.sanitize_sql_like(@keywords)}%").
+        or(Spree::Product.
+        where("spree_products.description LIKE :keyword", keyword: "%#{ActiveRecord::Base.sanitize_sql_like(@keywords)}%")).
+        includes(master: [:default_price, :images])
+    end
+  end
 end
